447892768a2da4ba25693143c484ee609b786dd9
Jay Vitela <99625446+JayV101@users.noreply.github.com>
2024-06-16 12:18:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -85,3 +85,6 @@ def print_all_paychecks():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
